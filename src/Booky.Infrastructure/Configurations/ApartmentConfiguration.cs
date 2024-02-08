4fa04af657792511c0f1d7e8996da9332746bdab
Alexandre Carvalho <carvalho2209@hotmail.com>
2024-02-07 16:53:55 -0800

    Creating the Domain entity configurations

---
+++ src/Booky.Infrastructure/Configurations/ApartmentConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Booky.Domain.Apartments;
+using Booky.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booky.Infrastructure.Configurations;
+
+public sealed class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+{
+    public void Configure(EntityTypeBuilder<Apartment> builder)
+    {
+        builder.ToTable("apartments");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.Address);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(200)
+            .HasConversion(name => name.Value, value => new Name(value));
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(2000)
+            .HasConversion(desc => desc.Value, value => new Description(value));
+
+        builder.OwnsOne(apart => apart.Price, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(apart => apart.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+    }
+}
---
+++ src/Booky.Infrastructure/Configurations/BookingConfigurations.cs
@@ -0,0 +1,52 @@
+﻿using Booky.Domain.Apartments;
+using Booky.Domain.Bookings;
+using Booky.Domain.Shared;
+using Booky.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Booky.Infrastructure.Configurations;
+
+public sealed class BookingConfigurations : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.PriceForPeriod, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.CleaningFee, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.AmenitiesUpCharge, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.TotalPrice, priceBuilder =>
+        {
+            priceBuilder.Property(money => money.Currency)
+                .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+        });
+
+        builder.OwnsOne(x => x.Duration);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(x => x.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(x => x.UserId);
+    }
+}
