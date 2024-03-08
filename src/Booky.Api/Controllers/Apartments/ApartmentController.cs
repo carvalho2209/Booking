1ccac63497d7476b8589be854ed94d1e203df7b2
Alexandre Carvalho <carvalho2209@hotmail.com>
2024-03-08 14:37:00 -0800

    Adding Controllers and endpoints

---
+++ src/Booky.Api/Controllers/Apartments/ApartmentController.cs
@@ -0,0 +1,27 @@
+﻿using Booky.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booky.Api.Controllers.Apartments;
+
+[ApiController]
+[Route("api/apartments")]
+public class ApartmentController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ApartmentController(ISender sender) => _sender = sender;
+
+    [HttpGet]
+    public async Task<IActionResult> SearchApartments(
+        DateOnly startDate,
+        DateOnly endDate,
+        CancellationToken cancellationToken)
+    {
+        var query = new SearchApartmentsQuery(startDate, endDate);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+}
